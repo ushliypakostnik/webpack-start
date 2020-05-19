458f5e808c66a8478441a9b1748a88d5e804a693
ushliypakostnik <levon.gambaryan@gmail.com>
2020-05-19 21:42:03 +0300

    add comment

--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -28,6 +28,7 @@ module.exports = {
         options: {
           name: '[path][name].[ext]',
           context: PATHS.src,
+          esModule: false, // fix for image paths
         },
       },
     }],
